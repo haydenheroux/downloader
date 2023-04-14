3167ef8f29ed2b28d600a972efc2f37eb15af2a2
Hayden Heroux <haydenheroux@gmail.com>
2023-04-14 17:56:48 -0400

    Document command-line options.

--- main.go
+++ main.go
@@ -8,17 +8,19 @@ import (
 )
 
 var (
-	DOWNLOAD_EXECUTABLE string
-	INPUT               string
-	OUT_DIR             string
-	OUT_FMT             string
+	downloaderExecutable string
+	outputExtension             string
+	outputFormat             string
+	inputFile               string
+	outputDirectory             string
 )
 
 func init() {
-	flag.StringVar(&DOWNLOAD_EXECUTABLE, "x", "yt-dlp", "youtube-dl executable, yt-dlp supported")
-	flag.StringVar(&INPUT, "i", "", "input file")
-	flag.StringVar(&OUT_DIR, "o", "out", "output directory")
-	flag.StringVar(&OUT_FMT, "f", "mp3", "output audio format")
+	flag.StringVar(&downloaderExecutable, "d", "yt-dlp", "Name of the executable to be used for downloading tracks. Only downloaders that use youtube-dl-compatible options are supported.")
+	flag.StringVar(&outputExtension, "e", "mp3", "Output audio format extension. Used for detecting downloaded files output.")
+	flag.StringVar(&outputFormat, "f", "mp3", "Output audio format. Used for specifying to the downloader which format to download.")
+	flag.StringVar(&inputFile, "i", "", "Input file. The file must contain lines with three tab-separated (TSV) fields, in this order: URL Artist(s) Title. Multiple artists can be included by delimiting with ampersands (&).")
+	flag.StringVar(&outputDirectory, "o", "", "Output directory. If the directory does not exist, it will be created. If this option is not specified, the current working directory is used.")
 }
 
 func downloadTo(downloader Downloader, track TrackInfo, directory string) (string, error) {
@@ -46,37 +48,37 @@ func main() {
 
 	logger := log.New(os.Stderr, "music_dl: ", 0)
 
-	if INPUT == "" {
-		logger.Fatalln("Usage: music_dl -i INPUT_FILE -o OUTPUT_DIRECTORY")
+	if inputFile == "" {
+		logger.Fatalln("Usage: music_dl -i inputFile")
 	}
 
 	downloader := YoutubeDLCompatibleDownloader{
-		Executable:      DOWNLOAD_EXECUTABLE,
-		Format:          OUT_FMT,
-		FormatExtension: OUT_FMT,
+		Executable:      downloaderExecutable,
+		Format:          outputFormat,
+		FormatExtension: outputFormat,
 	}
 
-	if err := mkdir(OUT_DIR); err != nil {
+	if err := mkdir(outputDirectory); err != nil {
 		logger.Fatalln("Creating directory")
 	}
 
 	tracks := []TrackInfo{}
 
-	lines, _ := linesIn(INPUT)
+	lines, _ := linesIn(inputFile)
 
 	for n, line := range lines {
-		track, _ := Parse(line)
+		track, _ := parse(line)
 		logger.Printf("Successfully parsed line %d: %s\n", n, track)
 		tracks = append(tracks, track)
 	}
 
 	for _, track := range tracks {
-		destination := filepath.Join(OUT_DIR, track.String())
+		destination := filepath.Join(outputDirectory, track.String())
 		if exists(destination) {
 			logger.Printf("Skipping %s because %s already exists\n", track, destination)
 			continue
 		}
-		destination, err := downloadTo(downloader, track, OUT_DIR)
+		destination, err := downloadTo(downloader, track, outputDirectory)
 		if err != nil {
 			logger.Fatalf("Failed to download %s because %v\n", destination, err)
 		}
