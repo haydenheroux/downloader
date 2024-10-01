299e13a9b9dd7a00c8630d71be2236669622fe8d
Hayden Heroux <haydenheroux@gmail.com>
2024-10-01 13:10:39 -0400

    refactor: Break actions into functions.

--- cmd/media/media.go
+++ cmd/media/media.go
@@ -23,21 +23,9 @@ func main() {
 		},
 		Commands: []*cli.Command{
 			{
-				Name:  "list",
-				Usage: "list reference keys",
-				Action: func(c *cli.Context) error {
-					references := c.StringSlice("reference")
-
-					referenceSet, err := resource.ParseFiles(references)
-					if err != nil {
-						return err
-					}
-
-					for _, key := range referenceSet.PrimaryKeys() {
-						fmt.Println(key)
-					}
-					return nil
-				},
+				Name:   "list",
+				Usage:  "list reference keys",
+				Action: listReferenceKeys,
 			},
 			{
 				Name:  "download",
@@ -72,66 +60,7 @@ func main() {
 						Required: true,
 					},
 				},
-				Action: func(c *cli.Context) error {
-					keys := c.Bool("keys")
-
-					references := c.StringSlice("reference")
-					inputs := c.StringSlice("input")
-
-					downloader := c.String("downloader")
-					output := c.String("output")
-					format := c.String("format")
-
-					referenceSet, err := resource.ParseFiles(references)
-					if err != nil {
-						return err
-					}
-
-					var inputKeys []resource.PrimaryKey
-
-					if keys {
-						inputKeys, err = resource.ParseKeyFiles(inputs)
-						if err != nil {
-							return err
-						}
-					} else {
-						inputSet, err := resource.ParseFiles(inputs)
-						if err != nil {
-							return err
-						}
-
-						referenceSet.AddAll(inputSet)
-
-						inputKeys = inputSet.PrimaryKeys()
-					}
-
-					dl := dler.CreateDownloader(downloader, format)
-
-					dl.SetOutputDirectory(output)
-
-					if _, err := os.Stat(output); err != nil {
-						if err := os.Mkdir(output, 0777); err != nil {
-							return err
-						}
-					}
-
-					for _, key := range inputKeys {
-						if !referenceSet.ContainsKey(key) {
-							return fmt.Errorf("key %s does not match any reference", key)
-						}
-
-						bestResource := referenceSet.Best(key)
-
-						log.Printf("started %s\n", dl.OutputLocation(bestResource))
-						err := dl.Download(bestResource)
-						if err != nil {
-							return err
-						}
-						log.Printf("finished %s\n", dl.OutputLocation(bestResource))
-					}
-
-					return nil
-				},
+				Action: downloadResources,
 			},
 		},
 	}
@@ -139,4 +68,79 @@ func main() {
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func listReferenceKeys(c *cli.Context) error {
+	references := c.StringSlice("reference")
+
+	referenceSet, err := resource.ParseFiles(references)
+	if err != nil {
+		return err
+	}
+
+	for _, key := range referenceSet.PrimaryKeys() {
+		fmt.Println(key)
+	}
+	return nil
+}
+
+func downloadResources(c *cli.Context) error {
+	keys := c.Bool("keys")
+
+	references := c.StringSlice("reference")
+	inputs := c.StringSlice("input")
+
+	downloader := c.String("downloader")
+	output := c.String("output")
+	format := c.String("format")
+
+	referenceSet, err := resource.ParseFiles(references)
+	if err != nil {
+		return err
+	}
+
+	var inputKeys []resource.PrimaryKey
+
+	if keys {
+		inputKeys, err = resource.ParseKeyFiles(inputs)
+		if err != nil {
+			return err
+		}
+	} else {
+		inputSet, err := resource.ParseFiles(inputs)
+		if err != nil {
+			return err
+		}
+
+		referenceSet.AddAll(inputSet)
+
+		inputKeys = inputSet.PrimaryKeys()
+	}
+
+	dl := dler.CreateDownloader(downloader, format)
+
+	dl.SetOutputDirectory(output)
+
+	if _, err := os.Stat(output); err != nil {
+		if err := os.Mkdir(output, 0777); err != nil {
+			return err
+		}
+	}
+
+	for _, key := range inputKeys {
+		if !referenceSet.ContainsKey(key) {
+			return fmt.Errorf("key %s does not match any reference", key)
+		}
+
+		bestResource := referenceSet.Best(key)
+
+		log.Printf("started %s\n", dl.OutputLocation(bestResource))
+		err := dl.Download(bestResource)
+		if err != nil {
+			return err
+		}
+		log.Printf("finished %s\n", dl.OutputLocation(bestResource))
+	}
+
+	return nil
 }
